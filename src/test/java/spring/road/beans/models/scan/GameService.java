3c9ff9a81f3dcf9881d04c31932c840882962137
lijinpeng <lijinpeng-it@bestpay.com.cn>
2019-04-17 22:37:09 +0800

    spring增加aop切入点 表达式 和 方法匹配器接口

--- src/test/java/spring/road/beans/models/scan/GameService.java
+++ src/test/java/spring/road/beans/models/scan/GameService.java
@@ -1,5 +1,6 @@
 package spring.road.beans.models.scan;
 
+import lombok.extern.slf4j.Slf4j;
 import spring.road.beans.annation.Autowired;
 import spring.road.beans.models.Person;
 import spring.road.beans.models.scan.impl.Boss;
@@ -10,11 +11,16 @@ import spring.road.stereotype.Component;
  * Created by Shanghai on 2019/4/8.
  */
 @Component(value = "gameService")
-public class GameService extends Boss{
+@Slf4j
+public class GameService extends Boss {
     @Autowired
     public Person person;
 
     public Person getPerson() {
         return person;
+    }
+
+    public void playGames() {
+        log.info("person-name:{} play games", person.getName());
     }
 }
